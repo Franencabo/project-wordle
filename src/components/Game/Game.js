30dd6bb25f136b0064463b24ba27d495258ac0b5
Fran <franencabo@gmail.com>
2024-07-25 19:32:41 +0200

    Exercise 3: Guess Slots

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,7 +3,7 @@ import React from "react";
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessInput from "../GuessInput/GuessInput";
-import Guesses from "../Guesses/Guesses";
+import Guesses from "../GuessResults/GuessesResults";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
--- src/components/GuessResults/GuessesResults.js
+++ src/components/GuessResults/GuessesResults.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Guess from "../Guess/Guess";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { range } from "../../utils";
 
 function Guesses({ guesses }) {
   return (
@@ -7,6 +10,9 @@ function Guesses({ guesses }) {
         <p key={index} className="guess">
           {guess}
         </p>
+      ))}
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={guesses[num]} />
       ))}
     </div>
   );
