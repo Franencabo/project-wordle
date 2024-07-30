a7949150e7c995c2ee12bfec391203a5caf9c10b
Fran <franencabo@gmail.com>
2024-07-30 20:35:02 +0200

    Exercise 4: Game Logic - Josh Solution

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,19 +2,22 @@ import React from "react";
 import { range } from "../../utils";
 import { checkGuess } from "../../game-helpers";
 
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : "cell";
+  return <span className={className}>{letter}</span>;
+}
+
 function Guess({ value, answer }) {
   const result = checkGuess(value, answer);
-  const letterClass = result ? result.map((item, index) => item.status) : "";
 
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span
-          className={`cell ${letterClass[num] ? letterClass[num] : ""}`}
+        <Cell
           key={num}
-        >
-          {value ? value[num] : undefined}
-        </span>
+          letter={result ? result[num].letter : undefined}
+          status={result ? result[num].status : undefined}
+        />
       ))}
     </p>
   );
